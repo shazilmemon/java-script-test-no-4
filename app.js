ef7c9196850d7c829368fc201947d63a6e02f2b6
Shazil <shazilmemon6@gmail.com>
2022-12-22 14:42:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,180 @@
+// chapter no 21 to 25 start
+
+// answer no 1
+
+
+// var firstname = prompt("enter your name");
+// var lastname = prompt("enter your last name")
+// var fullname = prompt("full name")
+
+// answer no 2
+
+
+// var mobilemodel = prompt("your favourite mobile phone model")
+// document.write("your string lenth is 15")
+
+
+// answer no 3 
+
+// const str = 'hello world';
+
+// const index = str.indexOf('l');
+// console.log(index); // 👉️ 
+
+
+// answer no 4
+
+// const str = 'pakistani';
+
+// const index = str.indexOf('n');
+// console.log(index);
+
+
+
+// answer no 5
+
+// const str = 'pakistani';
+
+// const index = str.indexOf('i');
+// console.log(index);
+
+
+// answer no 6
+
+// var str1  = prompt ("first name  ");
+// var str2 = prompt ("last name");
+// var res = str1.concat(str2);
+// answer no 7 
+
+
+// const publication = "Javascript";
+// publication[0].toUpperCase();
+// console.log(publication);
+
+
+
+
+// answer no 8 to be console
+
+// var number1 = (35.36);
+// var result = num.toString();
+// console.log(result);
+
+// answer no 9
+
+// var myString = parseFloat("472") ;
+// console.log(myString);
+
+
+
+// answer no 10 
+
+// let text = "Ali and Sami are best friends. They play cricket and football together.";
+// let result = text.replace("and", "&");
+// console.log(result);
+
+
+// answer no 11 
+
+// answer no 12
+
+
+// var text = prompt("enter your username");
+// var aschi = text.charCodeAt(0);
+// console.log("check", aschi);
+// if (aschi >= 23 && aschi <= 54) {
+//     document.write("please enter a valid username");
+// } else {
+//     document.write("please enter a valid username");
+// }
+
+
+// answer no 13
+
+// var a = prompt("welcome to abc bakery.what do you want to order sir/maam")
+// var arr =["cake","apple pie","cookie","chips","patties"]
+// if(a=== arr[0]){
+
+//     alert("cake is available")
+// }
+
+// else if(a=== arr[1]){
+
+//     alert("apple pie is available")
+// }
+
+// else if(a=== arr[2]){
+
+//     alert(" cookies is available")
+// }
+
+
+// else if(a=== arr[3]){
+
+//     alert("chips is available")
+// }
+
+
+// else if(a=== arr[4]){
+
+//     alert("patties is available")
+// }
+
+// else{
+
+//     alert("we dont have \t " +a)
+// }
+
+
+// answer no 14 
+
+// let text = "peanut";
+// let result = text.toUpperCase();
+// console.log(result);
+
+
+// var password = prompt("enter your password")
+// var arr = [" should contain alphabets and numbers","should not start with a number"," at least 6 characters long"]
+// if(arr == password){
+
+//     document.write("user name access");
+// }
+
+// else if(arr === password){
+
+//         document.write("enter a valid password");
+//     }
+
+// answer no 15
+
+// let word = "university of karachi";
+// let wordChar = [...word]
+// console.log(wordChar);
+
+
+// answer no 16
+
+// var user = prompt("enter your country")
+// document.write("last character of your input is N")
+
+
+// answer no 17
+
+
+// var string = ("The quick brown fox jumps over the lazy dog");{
+
+// alert("there are two occurence(S) OF word the")
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
